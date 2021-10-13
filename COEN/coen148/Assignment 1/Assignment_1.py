33bb27a492cceeed7e98a0ba7c4882f1bf73ac4c
Daren Liu <dliu3@scu.edu>
2021-10-12 17:06:51 -0700

    Fixed anti-aliasing for q3

--- COEN/coen148/Assignment 1/Assignment_1.py
+++ COEN/coen148/Assignment 1/Assignment_1.py
@@ -64,45 +64,21 @@ def scan_line(pixels):
                 last = i
         fill_row(pixels, first, last, j)
 
-def circle_point_blurred(pixels, x, y):
-    #Draw 7 symmetric pixels, given one pixel
-    #Shift to the middle of the image
-    x += 200
-    y += 200
-
-    pixels[x, y] = blurred
-    pixels[-x, y] = blurred
-    pixels[-x, -y] = blurred
-    pixels[x, -y] = blurred
-    pixels[y, x] = blurred
-    pixels[-y, x] = blurred
-    pixels[-y, -x] = blurred
-    pixels[y, -x] = blurred
-
-def anti_alias(pixels, radius):
-    #Initial values
-    d = (5/4) - radius 
-    x = 0
-    y = radius
-
-    #Fill in initial point
-    circle_point_blurred(pixels, x, y)
-
-    #Midpoint algorithm
-    while x < y:
-        if d < 0:
-            d += 2*x + 3
-        else:
-            d += (2*x) - (2*y) - 5
-            y -= 1
-        x += 1
-        circle_point_blurred(pixels, x, y) #once the pixel is decided, fill in the point
+def anti_alias(pixels):
+    for j in range(0, col - 1):
+        for i in range(0, row - 1):
+            if pixels[i, j] != color and pixels[i + 1, j] == color:
+                pixels[i, j] = blurred
+            elif pixels[i, j] == color and pixels[i + 1, j] != color:
+                pixels[i + 1, j] = blurred
+            elif pixels[i, j] != color and pixels[i, j + 1] == color:
+                pixels[i, j] = blurred
+            elif pixels[i, j] == color and pixels[i, j + 1] != color:
+                pixels[i, j + 1] = blurred
 
 draw_circle(pixels, 100)
 img.save('q1.png')
-#Draw a blurred circle with 1 larger and smaller radius than the original circle
-anti_alias(pixels, 101)
-anti_alias(pixels, 99)
-img.save('q3.png')
 scan_line(pixels)
 img.save('q2.png')
+anti_alias(pixels)
+img.save('q3.png')
